7b80d3a1356ea268ad71443c2420415feb497985
NathanFelipe99 <nathanfguerlando@gmail.com>
2023-02-05 22:56:09 -0300

    testing createBookCategoryUseCase

--- src/application/bookCategory/createBookCategoryUseCase/CreateBookCategoryUseCase.spec.ts
+++ src/application/bookCategory/createBookCategoryUseCase/CreateBookCategoryUseCase.spec.ts
@@ -1,5 +1,6 @@
 import { IBookCategoryRepository } from "../../../domain/bookCategory/interfaces/IBookCategoryRepository";
 import { BookCategoryRepositoryInMemory } from "../../../infra/database/repositories/bookCategory/BookCategoryRepositoryInMemory";
+import { BookCategoryProps } from "../../../shared/utils/types/bookCategory/bookCategory.types";
 import { CreateBookCategoryUseCase } from "./CreateBookCategoryUseCase";
 
 let bookCategoryRepository: IBookCategoryRepository;
@@ -11,10 +12,30 @@ describe("Create book category use case test", () => {
         createBookCategoryUseCase = new CreateBookCategoryUseCase(bookCategoryRepository);
     });
     it("should be able to create a new book category", async () => {
+        const newBookCategoryObj: BookCategoryProps = {
+            name: "Crime"
+        };
 
+        const bookCategory = await createBookCategoryUseCase.execute(newBookCategoryObj);
+
+        expect(bookCategory.id).toBeDefined();
+        expect(bookCategory.isActive).toEqual(true);
+        expect(bookCategory.name).toEqual(newBookCategoryObj.name);
     });
 
     it("shouldn't be able to create a new book category", async () => {
+        const firstBookCategoryObj: BookCategoryProps = {
+            name: "Crime"
+        };
 
+        await createBookCategoryUseCase.execute(firstBookCategoryObj);
+
+        const secondBookCategory: BookCategoryProps = {
+            name: "Crime"
+        };
+
+        expect(createBookCategoryUseCase.execute(secondBookCategory))
+            .rejects
+            .toThrowError("This category already exists!");
     });
 });
